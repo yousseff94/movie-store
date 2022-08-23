695fde6b36aa1e96ba08363e2a74110c00db77d3
kasraouiyoussef <elkasraouiyoussef@gmail.com>
2022-08-23 15:48:02 +0200

    feat: movie-store axios and asychrone

--- src/components/movieList.jsx
+++ src/components/movieList.jsx
@@ -1,6 +1,11 @@
 import Movie from "./movie";
 import React, { useState } from "react";
 import { useEffect } from "react";
+import axios from "axios";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 export default function MovieList(props) {
   const [img, setImg] = useState("");
@@ -8,34 +13,97 @@ export default function MovieList(props) {
   const [genre, setGenre] = useState("");
   const [rate, setRate] = useState("");
 
-  const [listfilm, setListfilm] = useState([""]);
-  const [error, setError] = useState(null);
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "31d22a1a74mshced98b321ccbfb9p170badjsnf821ac5cc879",
-      "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
-    },
+  const [listfilm, setListfilm] = useState([]);
+  const [error, setError] = useState();
+
+
+////////////////////////methode axios with asychn and awaite///////////////////////////
+ 
+
+
+  const getMovieRequest = async () => {
+    const url = "http://omdbapi.com/?s=marvel&apikey=339b64b7";
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    console.log(responseJson);
+    setListfilm(responseJson.Search);
   };
+
   useEffect(() => {
-    fetch(
-      "https://online-movie-database.p.rapidapi.com/auto-complete?q=game%of%thr",
-      options
-    )
-      .then((response) => response.json())
-     
-      .then((response) => {
-        setListfilm(response.d);
-      })
-      .catch((err) => {
-        setError(err.message);
-      });
+    getMovieRequest();
   }, []);
+
+
+// methode axios///////////////////////////////////////////////////////////
+  // const options = {
+  //   method: "GET",
+  //   url: "https://online-movie-database.p.rapidapi.com/auto-complete",
+  //   params: { q: "game of thr" },
+  //   headers: {
+  //     "X-RapidAPI-Key": "5ec832a2a2msh7e771a1a6dd532ep11a190jsn95e8519948bb",
+  //     "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
+  //   },
+  // };
+  // useEffect(() => {
+  //   axios.request(options)
+  //       .then(function (response) {
+  //       setListfilm(response.data.d);
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+  // }, []);
+ //methode fetch//////////////////////////////////////////////////////////
+  //useEffect(() => {
+   
+    // fetch(
+    //   "https://online-movie-database.p.rapidapi.com/auto-complete?q=game%of%thr",
+    //   options
+    // )
+    //   .then((response) => response.json())
+   
+    //   .then((response) => {
+    //     setListfilm(response.d);
+    //   })
+    //   .catch((err) => {
+    //     setError(err.message);
+    //   });
+   
   return (
     <div>
-      {listfilm.map((movie, index) => {
-        return <Movie key={index} img={movie.imageUrl} genre={movie.l} title={movie.q} />;
-      })}
+      {/* {listfilm.map((movie, index) => {
+        return (
+          <Movie
+            key={index}
+            img={movie.i && movie.i.imageUrl}
+            genre={movie.l}
+            title={movie.q}
+          />
+        );
+      })} */}
+
+    {listfilm.map((movie,index)=>{
+      return(
+        <div>
+
+
+     <Container>
+      
+      <Row>
+        <Col> <Movie
+        key={index}
+         img={movie.Poster}
+         title={movie.Type}
+          genre={movie.Year}
+        /></Col>
+        
+      </Row>
+    </Container>
+
+        </div>
+        
+      )
+    })}
 
       {/* <Movie
           img={
